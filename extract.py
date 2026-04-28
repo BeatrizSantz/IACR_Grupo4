6dbbde0247654cb7c1f5d9d0aed4aba1a9ed879b
JulietaSequeira <jujusequeira@gmail.com>
2026-04-28 20:58:34 +0100

    extract.py
    para "fazermos" os datasets

---
+++ extract.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+input_dir = "data/lfw-deepfunneled/lfw-deepfunneled"
+output_dir = "data/lfw_filtered"
+min_images = 5
+
+# limpar pasta de saída
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir)
+
+os.makedirs(output_dir)
+
+count_people = 0
+
+for person in os.listdir(input_dir):
+    person_path = os.path.join(input_dir, person)
+
+    if not os.path.isdir(person_path):
+        continue
+
+    images = [f for f in os.listdir(person_path)
+              if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+
+    print(person, len(images))  # debug
+
+    if len(images) >= min_images:
+        shutil.copytree(person_path, os.path.join(output_dir, person))
+        count_people += 1
+
+print(f"Total de pessoas usadas: {count_people}")
